aea35b0bbd18ee6816935effa746cd1dec9bcadf
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2023-03-12 17:23:40 +0100

    Update :: Addition @ InvertedResidualBlock

--- main.py
+++ main.py
@@ -28,3 +28,69 @@ phi_values = {
     "b7": (6, 600, 0.5),
 }
 
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            groups=groups,
+            bias=False,
+        )
+        self.bn = nn.BatchNorm2d(out_channels, momentum=0.01, eps=1e-3)
+        self.silu = nn.SiLU()  # or nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.silu(x)
+        return x
+
+
+class SqueezeExcite(nn.Module):
+    def __init__(self, in_channels, reduced_dim):
+        super(SqueezeExcite, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1), # or GlobalAvgPool2d, C x H x W -> C x 1 x 1
+            nn.Conv2d(in_channels, reduced_dim, kernel_size=1),
+            nn.SiLU(),
+            nn.Conv2d(reduced_dim, in_channels, kernel_size=1),
+            nn.Sigmoid(), # or nn.Softmax(dim=1) for multiclass tasks
+        )
+
+    def forward(self, x):
+        return x * self.se(x)
+
+
+class InvertedResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, expand_ratio):
+        super(InvertedResidualBlock, self).__init__()
+        self.use_res_connect = stride == 1 and in_channels == out_channels
+
+        hidden_dim = in_channels * expand_ratio
+        self.expand = in_channels != hidden_dim
+        if self.expand:
+            self.expand_conv = CNNBlock(
+                in_channels, hidden_dim, kernel_size=3, stride=1, padding=1
+            )
+
+        self.conv = nn.Sequential(
+            CNNBlock(
+                hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim
+            ),
+            SqueezeExcite(),
+            nn.Conv2d(hidden_dim, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(out_channels, momentum=0.01, eps=1e-3),
+        )
+
+    def forward(self, x):
+        if self.expand:
+            x = self.expand_conv(x)
+        x = self.conv(x)
+        if self.use_res_connect:
+            return x + x
+        return x
