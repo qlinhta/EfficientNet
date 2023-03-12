2ac4200801c0982a3b6514688b1ba1605ad18d84
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2023-03-12 17:37:18 +0100

    Update :: Addition @ InvertedResidualBlock

--- main.py
+++ main.py
@@ -91,3 +91,57 @@ class InvertedResidualBlock(nn.Module):
             return x
         binary_tensor = torch.rand(x.shape[0], 1, 1, 1, device=x.device) < self.survival_prob
         return torch.div(x, self.survival_prob) * binary_tensor
+
+    def forward(self, inputs):
+        x = self.expand_conv(inputs) if self.expand else inputs
+        if self.use_residual:
+            return self.stochastic_depth(self.conv(x)) + inputs
+        else:
+            return self.conv(x)
+
+
+class EfficientNet(nn.Module):
+    def __init__(self, version, num_classes):
+        super(EfficientNet, self).__init__()
+        width_factor, depth_factor, dropout_rate = self.calculate_factors(version)
+        last_channels = ceil(1280 * width_factor)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.features = self.create_features(width_factor, depth_factor, last_channels)
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(last_channels, num_classes),
+        )
+
+    def calculate_factors(self, version, alpha=1.2, beta=1.1):
+        phi, res, drop_rate = phi_values[version]
+        depth_factor = alpha ** phi
+        width_factor = beta ** phi
+        return width_factor, depth_factor, drop_rate
+
+    def create_features(self, width_factor, depth_factor, last_channels):
+        channels = int(32 * width_factor)
+        features = [CNNBlock(3, channels, kernel_size=3, stride=2, padding=1)]
+        in_channels = channels
+
+        for expand_ratio, channels, repeats, stride, kernel_size in base_model:
+            out_channels = 4 * ceil(int(channels * width_factor) / 4)
+            layers_repeats = ceil(repeats * depth_factor)
+
+            for layer in range(layers_repeats):
+                features.append(
+                    InvertedResidualBlock(
+                        in_channels,
+                        out_channels,
+                        kernel_size,
+                        stride if layer == 0 else 1,
+                        padding=kernel_size // 2,
+                        expand_ratio=expand_ratio,
+                    )
+                )
+                in_channels = out_channels
+        features.append(CNNBlock(in_channels, last_channels, kernel_size=1, stride=1, padding=0))
+        return nn.Sequential(*features)
+
+    def forward(self, x):
+        x = self.pool(self.features(x))
+        return self.classifier(x.view(x.shape[0], -1))
